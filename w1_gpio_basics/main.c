6288fd0374ab492cffed97c377f13d08965d6ea0
Evgeniy Morozov <zhe3145@yandex.ru>
2017-11-21 23:23:57 +0300

    Blink leds on the extension board using timers. Closed #2

--- w1_gpio_basics/main.c
+++ w1_gpio_basics/main.c
@@ -4,12 +4,17 @@
 
 int main(void)
 {
-  GPIO_InitTypeDef  GPIO_InitStructure;
+  GPIO_InitTypeDef GPIO_InitStructure;
+
+  /* LEDs array to toggle between them */
+  uint16_t leds[3] = { LED_RED, LED_GREEN, LED_BLUE };
+  /* LED to toggle during iteration */
+  uint8_t  current_led = 0;
 
   /* Enable peripheral clock for LEDs port */
   RCC_AHB1PeriphClockCmd(LEDS_CLK, ENABLE);
 
-  /* Init leds */
+  /* Init LEDs */
   GPIO_InitStructure.GPIO_Pin   = LED_RED | LED_GREEN | LED_BLUE;
   GPIO_InitStructure.GPIO_Mode  = GPIO_Mode_OUT;
   GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
@@ -17,22 +22,19 @@ int main(void)
   GPIO_InitStructure.GPIO_PuPd  = GPIO_PuPd_NOPULL;
   GPIO_Init(LEDS_PORT, &GPIO_InitStructure);
 
+  /* Turn all the leds off */
+  GPIO_SetBits(LEDS_PORT, LED_RED | LED_GREEN | LED_BLUE);
+
   while (1)
   {
-	  int i;
+    int i;
 
-      /* Switch LED on */
-      GPIO_SetBits(LEDS_PORT, LED_RED | LED_GREEN | LED_BLUE);
-	  for (i=0; i < SWITCH_DELAY; i++)
-	  {
-	    /* empty cycle */
-	  }
+    /* Switch the LED on */
+    GPIO_ResetBits(LEDS_PORT, leds[current_led % 3]);
+    for (i = 0; i < SWITCH_DELAY; i++);
 
-	  /* Switch LED off */
-	  GPIO_ResetBits(LEDS_PORT, LED_RED | LED_GREEN | LED_BLUE);
-	  for (i=0; i < SWITCH_DELAY; i++)
-	  {
-	    /* empty cycle */
-	  }
+    /* Switch the LED off */
+    GPIO_ResetBits(LEDS_PORT, leds[current_led++ % 3]);
+    for (i = 0; i < SWITCH_DELAY; i++);
   }
 }
