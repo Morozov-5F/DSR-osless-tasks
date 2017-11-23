ad8c7b5aca1e28d403d444142e8827a03f68e433
Eugene Morozov <zhe3145@yandex.ru>
2017-11-24 01:01:36 +0300

    Change LED brightness by Cosine function Adresses #6

--- w4_pwm/main.c
+++ w4_pwm/main.c
@@ -5,92 +5,128 @@
 /* static int8_t   leds_direction; */
 
 /* PWM frequency */
-#define PWM_FREQUENCY_HZ      40
+#define PWM_FREQUENCY_HZ      50
 
-static
-int configure_leds()
+uint32_t time;
+
+static int
+configure_leds(void)
 {
-   GPIO_InitTypeDef gpio_init;
-   GPIO_StructInit(&gpio_init);
-   RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
+    GPIO_InitTypeDef gpio_init;
+    GPIO_StructInit(&gpio_init);
+    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
 
-   GPIO_PinAFConfig(GPIOA, GPIO_PinSource8, GPIO_AF_TIM1);
-   /* GPIO_PinAFConfig(GPIOA, GPIO_PinSource9, GPIO_AF_TIM1); */
-   /* GPIO_PinAFConfig(GPIOA, GPIO_PinSource10, GPIO_AF_TIM1); */
+    GPIO_PinAFConfig(GPIOA, GPIO_PinSource8, GPIO_AF_TIM1);
+    /* GPIO_PinAFConfig(GPIOA, GPIO_PinSource9, GPIO_AF_TIM1); */
+    /* GPIO_PinAFConfig(GPIOA, GPIO_PinSource10, GPIO_AF_TIM1); */
 
-   /* Initialize GPIOs for an alternative function (TIM1 PWM output) */
-   gpio_init.GPIO_Pin   = GPIO_Pin_8 /* | GPIO_Pin_9 | GPIO_Pin_10 */;
-   gpio_init.GPIO_Mode  = GPIO_Mode_AF;
-   gpio_init.GPIO_OType = GPIO_OType_PP;
-   gpio_init.GPIO_Speed = GPIO_Speed_100MHz;
-   gpio_init.GPIO_PuPd  = GPIO_PuPd_NOPULL;
-   GPIO_Init(GPIOA, &gpio_init);
-   return 0;
+    /* Initialize GPIOs for an alternative function (TIM1 PWM output) */
+    gpio_init.GPIO_Pin   = GPIO_Pin_8; /* | GPIO_Pin_9 | GPIO_Pin_10 */;
+    gpio_init.GPIO_Mode  = GPIO_Mode_AF;
+    gpio_init.GPIO_OType = GPIO_OType_PP;
+    gpio_init.GPIO_Speed = GPIO_Speed_100MHz;
+    gpio_init.GPIO_PuPd  = GPIO_PuPd_NOPULL;
+    GPIO_Init(GPIOA, &gpio_init);
+    return 0;
 }
 
-static
-int configure_timers()
+static int
+configure_timers(void)
 {
-   /* Timer  */
-   TIM_TimeBaseInitTypeDef tim_init;
+    /* Timer  */
+    TIM_TimeBaseInitTypeDef tim_init;
 
-   TIM_TimeBaseStructInit(&tim_init);
+    TIM_TimeBaseStructInit(&tim_init);
 
-   RCC_APB2PeriphClockCmd(RCC_APB2Periph_TIM1, ENABLE);
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_TIM1, ENABLE);
 
-   tim_init.TIM_Prescaler         = 1680; /* One microsecond resolution */
-   tim_init.TIM_CounterMode       = TIM_CounterMode_Up;
-   tim_init.TIM_Period            = (uint16_t)(100000 / PWM_FREQUENCY_HZ - 1);
-   tim_init.TIM_ClockDivision     = TIM_CKD_DIV1;
-   tim_init.TIM_RepetitionCounter = 0;
-   TIM_TimeBaseInit(TIM1, &tim_init);
+    tim_init.TIM_Prescaler         = 1680; /* One microsecond resolution */
+    tim_init.TIM_CounterMode       = TIM_CounterMode_Up;
+    tim_init.TIM_Period            = (uint16_t)(100000 / PWM_FREQUENCY_HZ - 1);
+    tim_init.TIM_ClockDivision     = TIM_CKD_DIV1;
+    tim_init.TIM_RepetitionCounter = 0;
+    TIM_TimeBaseInit(TIM1, &tim_init);
 
-   return 0;
+    /* Second timer to track brigtness change */
+    TIM_TimeBaseStructInit(&tim_init);
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
+    tim_init.TIM_Prescaler         = 84;
+    tim_init.TIM_Period            = 999;
+    tim_init.TIM_ClockDivision     = TIM_CKD_DIV1;
+    tim_init.TIM_RepetitionCounter = 0;
+    TIM_ITConfig(TIM2, TIM_IT_Update, ENABLE);
+    TIM_TimeBaseInit(TIM2, &tim_init);
+
+    return 0;
 }
 
-static
-int configure_pwm()
+static int
+configure_interrupts(void)
 {
-   TIM_OCInitTypeDef tim_oc_init;
+    NVIC_InitTypeDef timer_nvic_init_structure;
 
-   TIM_OCStructInit(&tim_oc_init);
+    timer_nvic_init_structure.NVIC_IRQChannel           = TIM2_IRQn;
+    timer_nvic_init_structure.NVIC_IRQChannelPreemptionPriority = 0;
+    timer_nvic_init_structure.NVIC_IRQChannelSubPriority        = 1;
+    timer_nvic_init_structure.NVIC_IRQChannelCmd           = ENABLE;
+    NVIC_Init(&timer_nvic_init_structure);
 
-   tim_oc_init.TIM_OCMode      = TIM_OCMode_PWM1;
-   tim_oc_init.TIM_Pulse       = (uint16_t)(100000 / PWM_FREQUENCY_HZ - 1) * 0.1;
-   tim_oc_init.TIM_OutputState = TIM_OutputState_Enable;
-   tim_oc_init.TIM_OCPolarity  = TIM_OCPolarity_Low;
-
-   TIM_OC1Init(TIM1, &tim_oc_init);
-   TIM_OC1PreloadConfig(TIM1, TIM_OCPreload_Enable);
-   tim_oc_init.TIM_Pulse       = 0;
-   TIM_OC2Init(TIM1, &tim_oc_init);
-   TIM_OC2PreloadConfig(TIM1, TIM_OCPreload_Enable);
-   tim_oc_init.TIM_Pulse       = 0;
-   TIM_OC3Init(TIM1, &tim_oc_init);
-   TIM_OC3PreloadConfig(TIM1, TIM_OCPreload_Enable);
-
-   TIM_CtrlPWMOutputs(TIM1, ENABLE);
-   TIM_Cmd(TIM1, ENABLE);
-
-   return 0;
+    return 0;
 }
 
-void EXTI0_IRQHandler(void)
+static int
+configure_pwm(void)
 {
-   if (EXTI_GetITStatus(BTN_LEFT_EXTI_LINE) != RESET)
-   {
-      EXTI_ClearITPendingBit(BTN_LEFT_EXTI_LINE);
+    TIM_OCInitTypeDef tim_oc_init;
 
-   }
+    TIM_OCStructInit(&tim_oc_init);
+
+    tim_oc_init.TIM_OCMode      = TIM_OCMode_PWM1;
+    tim_oc_init.TIM_Pulse       = (uint16_t)(100000 / PWM_FREQUENCY_HZ - 1) * 0;
+    tim_oc_init.TIM_OutputState = TIM_OutputState_Enable;
+    tim_oc_init.TIM_OCPolarity  = TIM_OCPolarity_Low;
+
+    TIM_OC1Init(TIM1, &tim_oc_init);
+    TIM_OC1PreloadConfig(TIM1, TIM_OCPreload_Enable);
+
+    TIM_CtrlPWMOutputs(TIM1, ENABLE);
+
+    return 0;
+}
+
+static int
+start_timers(void)
+{
+    time = 0;
+    TIM_Cmd(TIM1, ENABLE);
+    TIM_Cmd(TIM2, ENABLE);
+}
+
+void
+TIM2_IRQHandler(void)
+{
+
+    if (TIM_GetITStatus(TIM2, TIM_IT_Update) != RESET)
+    {
+	TIM_ClearITPendingBit(TIM2, TIM_IT_Update);
+
+	time++;
+	uint16_t period    = 2000;
+	/* uint16_t pwm_value = (float)(abs(-(time % period) + period / 2)) *  2 / period * (100000 / PWM_FREQUENCY_HZ - 1); */
+	uint16_t pwm_value = (cos(time * 6.28 / period) + 1) / 2.0f * (100000 / PWM_FREQUENCY_HZ - 1);
+
+	TIM_SetCompare1(TIM1, pwm_value);
+    }
 }
 
 int main(void)
 {
-   configure_timers();
-   configure_pwm();
+    configure_timers();
+    configure_interrupts();
+    configure_pwm();
 
-   configure_leds();
-   /* configure_buttons(); */
+    configure_leds();
+    start_timers();
 
-   for (;;);
+    for (;;);
 }
